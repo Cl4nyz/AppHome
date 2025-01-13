c2b4d3601895ab682a163048a95f9aedd17820ba
Yan <supremoyan@gmail.com>
2025-01-13 20:22:47 -0300

    Seta de voltar e 'x'

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -12,32 +12,59 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        centerTitle: true,
-        title: Text(
-          'Elevadores',
-          style: TextStyle(
-            color: AZULESCURO,
-            fontSize: 22,
-            fontWeight: FontWeight.bold
-          )
-          ),
-          leading: Container(
-            //width: 10,
+      appBar: appBar(),
+    );
+  }
+
+  AppBar appBar() {
+    return AppBar(
+      backgroundColor: Colors.white,
+      centerTitle: true,
+      title: Text(
+        'Elevadores',
+        style: TextStyle(
+          color: AZULESCURO,
+          fontSize: 22,
+          fontWeight: FontWeight.bold
+        )
+        ),
+        leading: GestureDetector(
+          onTap: () {},
+          child: Container(
             margin: EdgeInsets.all(10),
             alignment: Alignment.center,
-            child: Image.asset(
-              'assets/icons/home.png',
-              width: 15,
-              height: 15,
-              ),
+            decoration: BoxDecoration(
+              color: Color(0xffF7F8F8),  // Color not constant, use 0xff...
+              borderRadius: BorderRadius.circular(10)
+            ),
+            child: SvgPicture.asset(
+              'assets/icons/left-arrow.svg',
+              height: 20,
+              width: 20
+            ),
+          ),
+        ),
+        actions: [
+          GestureDetector(
+            onTap: () {
+
+            },
+            child: Container(
+            margin: EdgeInsets.all(10),
+            alignment: Alignment.center,
+            width: 37,
+            child: SvgPicture.asset(
+              'assets/icons/x.svg',
+              height: 13,
+              width: 13
+            ),
             decoration: BoxDecoration(
               color: Color(0xffF7F8F8),  // Color not constant, use 0xff...
               borderRadius: BorderRadius.circular(10)
             )
           ),
-      ),
+        )
+      ]
     );
   }
 }
