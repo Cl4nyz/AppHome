607100184dbd4e1bea30a881cd70bc32621ae8ba
Yan <supremoyan@gmail.com>
2025-01-15 00:50:05 -0300

    Barra de pesquisa

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -5,6 +5,7 @@ import 'package:flutter_svg/flutter_svg.dart';
 
 const AZULCLARO = Color.fromARGB(255, 32, 143, 167);
 const AZULESCURO = Color.fromARGB(255, 11, 55, 94);
+const TAM = 30.0;
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -15,18 +16,74 @@ class HomePage extends StatelessWidget {
       appBar: appBar(),
       body: Column(
         children: [
-          Container(
-            
-            child: TextField(
-              decoration: InputDecoration(
-                filled: true,
-                fillColor: Colors.white
-              ),
-            ),
-          )
+          _searchField()
         ]
       )
     );
+  }
+
+  Container _searchField() {
+    return Container(
+          margin: EdgeInsets.only(top:40, left:20, right:20),
+          decoration: BoxDecoration(
+            boxShadow: [
+              BoxShadow(
+                color: Color(0xff101617).withOpacity(0.11),
+                blurRadius: 40,
+                spreadRadius: 0.0
+              )
+            ]
+          ),
+          child: TextField(
+            decoration: InputDecoration(
+              filled: true,
+              fillColor: Colors.white,
+              contentPadding: EdgeInsets.all(15),
+              hintText: 'Search Pancake',
+              hintStyle: TextStyle(
+                color: Color(0xffDDDADA),
+                fontSize: 14,
+              ),
+              prefixIcon: Container(
+                height: TAM,
+                width: TAM,
+                child: Padding(
+                  padding: const EdgeInsets.all(12),
+                  child: SvgPicture.asset('assets/icons/search.svg'),
+                ),
+              ),
+              suffixIcon: Container(
+                width: TAM*2,
+                height: TAM,
+                child: IntrinsicHeight(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      VerticalDivider(
+                        color: Colors.black,
+                        indent: 10,
+                        endIndent: 10,
+                        thickness: 0.1,
+                      ),
+                      Container(
+                        width: TAM,
+                        height: TAM,
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: SvgPicture.asset('assets/icons/filter.svg'),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(15),
+                borderSide: BorderSide.none
+              )
+            ),
+          ),
+        );
   }
 
   AppBar appBar() {
